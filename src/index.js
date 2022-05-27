10fb984d78322d5d4132a10f73ee6a409f48b43c
Carlos Guerra <guerragcarlos97@gmail.com>
2022-05-27 14:22:02 +0200

    Redux configured globally & types

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { Provider } from 'react-redux'
+import { store } from './store'
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+
+)
