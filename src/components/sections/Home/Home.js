f10052d8ee36742d2fbb8debf7ffee477379c35a
Carlos Guerra <guerragcarlos97@gmail.com>
2022-05-27 14:22:22 +0200

    Home & Posts fixes

--- src/components/sections/Home/Home.js
+++ src/components/sections/Home/Home.js
@@ -1,21 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import memories from '../../../assets/memories.png'
 
-import { PostsLayout } from '../Posts/PostsLayout'
-
 import { AppBar, Container } from '@material-ui/core'
 import { Image } from '../../common/Image'
+import { PostsLayout } from '../Posts/PostsLayout'
 import { Text } from '../../common/Text'
 
 export const Home = () => {
   return (
     <Container maxWidth='lg'>
-      <AppBar color='inherit' position='static'>
+      <StyledAppBar color='inherit' position='static'>
         <Text variant='h2'>Memories</Text>
         <Image alt='memories' height='60' src={memories} width='60' />
-      </AppBar>
+      </StyledAppBar>
       <PostsLayout />
     </Container>
   )
 }
+
+const StyledAppBar = styled(AppBar)`
+  align-items: center;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 30px 0;
+`
--- src/components/sections/Posts/Posts.js
+++ src/components/sections/Posts/Posts.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
 import { Post } from './Post'
+import { Text } from '../../common/Text'
 
 export const Posts = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+
+  }, [])
+
   return (
     <div>
+      <Text variant='h1'>Posts</Text>
       <Post />
       <Post />
     </div>
