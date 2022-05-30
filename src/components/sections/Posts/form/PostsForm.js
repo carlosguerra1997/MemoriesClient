0e3ee1ecbedc3db1f28cd4875a3f9be9fdb0405c
Carlos Guerra <guerragcarlos97@gmail.com>
2022-05-30 13:57:37 +0200

    Posts form changes

--- src/components/sections/Posts/form/PostsForm.js
+++ src/components/sections/Posts/form/PostsForm.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { TextField, Paper, Button} from '@material-ui/core'
+import { Paper } from '@material-ui/core'
 import { Text } from '../../../common/Text'
+import { TextFields } from '../../../common/TextField'
 
 export const PostsForm = () => {
-
   const handleSubmit = () => {
 
   }
@@ -13,14 +13,15 @@ export const PostsForm = () => {
   return (
     <Paper>
       <StyledForm autoComplete='off' onSubmit={handleSubmit}>
-
+        <Text variant='h6'>Añadir recuerdo</Text>
+        <TextFields label='Creator' name='creator' variant='outlined' />
       </StyledForm>
     </Paper>
   )
 }
 
 const StyledForm = styled.form`
-  display: 'flex',
-  flexWrap: 'wrap',
-  justifyContent: 'center',
+  display: flex;
+  flexWrap: wrap;
+  justifyContent: center;
 `
