a49db49f1efa45dac8634a1007a0e775d1b6f75d
carlosguerra1997 <guerragcarlos97@gmail.com>
2022-06-01 13:59:17 +0200

    Add post action & api cal

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,6 +1,15 @@
-import { GET_ALL_POSTS } from '../types'
+import { ADD_NEW_POST, GET_ALL_POSTS } from '../types'
 
-import { getPosts } from '../api/Posts'
+import { addNewPost, getPosts } from '../api/Posts'
+
+export const addPost = (post) => async dispatch => {
+  try {
+    const { data } = await addNewPost(post)
+    dispatch({ type: ADD_NEW_POST, payload: data })
+  } catch (error) {
+    throw new Error('Error... ', error)
+  }
+}
 
 export const getAllPosts = () => async dispatch => {
   try {
--- src/api/Posts.js
+++ src/api/Posts.js
@@ -1,5 +1,10 @@
 import { clienteAxios } from '../config/axios'
 
+export const addNewPost = async (post) => {
+  const data = await clienteAxios.post('/posts/', post)
+  return data
+}
+
 export const getPosts = async () => {
   const posts = await clienteAxios.get('/posts/')
   return posts
