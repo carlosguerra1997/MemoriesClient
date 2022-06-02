482fbf76c668b70a02a0a8bf82e2e8dddca7ae94
carlosguerra1997 <guerragcarlos97@gmail.com>
2022-06-02 12:28:15 +0200

    Posts structure

--- src/components/sections/Posts/Post.js
+++ src/components/sections/Posts/Post.js
@@ -1,9 +1,66 @@
 import React from 'react'
+import styled from 'styled-components'
 
-export const Post = () => {
+import { formatDate } from '../../../utils/formatDate'
+
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+
+import { Buttons } from '../../common/Button'
+import { Card, CardMedia } from '@material-ui/core'
+import { Text } from '../../common/Text'
+
+import { PostDetails } from './PostDetails'
+
+export const Post = ({ createdAt, creator, file, message, tags, title }) => {
   return (
-    <div>
-      Post
-    </div>
+    <StyledCard>
+      <StyledMedia image={file} title={title} />
+      <StyledInfo>
+        <Text align='left' variant='h6'>{creator}</Text>
+        <Text align='left' variant='body2'>{formatDate(createdAt)}</Text>
+      </StyledInfo>
+      <StyledMoreInfo>
+        <StyledButton onClick={() => {}}>
+          <MoreHorizIcon fontSize='medium' />
+        </StyledButton>
+      </StyledMoreInfo>
+      <PostDetails message={message} tags={tags} />
+    </StyledCard>
   )
 }
+
+const StyledCard = styled(Card)`
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  position: relative;
+`
+
+const StyledMedia = styled(CardMedia)`
+  padding-top: 60%;
+  background-color: rgba(0, 0, 0, 0.5);
+  background-blend-mode: darken;
+`
+
+const StyledInfo = styled.div`
+  color: white;
+  left: 20px;
+  position: absolute;
+  top: 20px;
+`
+
+const StyledMoreInfo = styled.div`
+  color: white;
+  right: 10px;
+  position: absolute;
+  top: 20px;
+`
+
+const StyledButton = styled(Buttons)`
+  background-color: transparent;
+  color: white;
+  :hover {
+    background-color: transparent;
+  }
+`
--- src/components/sections/Posts/Posts.js
+++ src/components/sections/Posts/Posts.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import styled from 'styled-components'
 
 import { getAllPosts } from '../../../actions/posts'
 
+import { CircularProgress, Grid } from '@material-ui/core'
 import { Post } from './Post'
-import { Text } from '../../common/Text'
 
 export const Posts = () => {
   const dispatch = useDispatch()
@@ -15,10 +16,21 @@ export const Posts = () => {
   }, [])
 
   return (
-    <div>
-      <Text variant='h1'>Posts</Text>
-      <Post />
-      <Post />
-    </div>
+    !posts.length
+      ? <CircularProgress />
+      : <StyledContainer container spacing={3}>
+        {
+          posts.map(post => (
+            <Grid item key={post._id} xs={12} sm={6}>
+              <Post {...post} />
+            </Grid>
+          ))
+        }
+      </StyledContainer>
   )
 }
+
+const StyledContainer = styled(Grid)`
+  align-items: center;  
+  display: flex;
+`
