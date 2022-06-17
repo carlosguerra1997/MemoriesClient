5eb97dd09fee029d303e59386c3b01fca0a816b8
carlosguerra1997 <guerragcarlos97@gmail.com>
2022-06-17 10:19:58 +0200

    Update & Set active post actions

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,6 +1,6 @@
-import { ADD_NEW_POST, GET_ALL_POSTS } from '../types'
+import { ADD_NEW_POST, GET_ALL_POSTS, SET_ACTIVE_POST, UPDATE_POST } from '../types'
 
-import { addNewPost, getPosts } from '../api/Posts'
+import { addNewPost, getPosts, updatePost } from '../api/Posts'
 
 export const addPost = (post) => async dispatch => {
   try {
@@ -15,6 +15,23 @@ export const getAllPosts = () => async dispatch => {
   try {
     const { data } = await getPosts()
     dispatch({ type: GET_ALL_POSTS, payload: data })
+  } catch (error) {
+    throw new Error('Error... ', error)
+  }
+}
+
+export const updateAPost = (postId, post) => async dispatch => {
+  try {
+    const { data } = await updatePost(postId, post)
+    dispatch({ type: UPDATE_POST, payload: data })
+  } catch (error) {
+    throw new Error('Error... ', error)
+  }
+}
+
+export const setActivePost = (postId) => async dispatch => {
+  try {
+    dispatch({ type: SET_ACTIVE_POST, payload: postId })
   } catch (error) {
     throw new Error('Error... ', error)
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import './index.css'
+
 import { Provider } from 'react-redux'
 import { store } from './store'
 
