867336d6d33e04a461524263d173bef756b91219
carlosguerra1997 <guerragcarlos97@gmail.com>
2022-06-18 12:55:03 +0200

    App routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
+import { Auth } from './components/sections/Auth/Auth'
 import { Home } from './components/sections/Home/Home'
 
 function App () {
-  return <Home />
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/auth' exact component={Auth} />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
