79bdaa52c5dee9d4b6d6bf719761126d882cfea9
carlosguerra1997 <guerragcarlos97@gmail.com>
2022-06-18 12:54:16 +0200

    Common components

---
+++ src/components/common/Container.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { Container } from '@material-ui/core'
+
+export const Containers = ({
+  children,
+  component,
+  maxWidth,
+  ...rest
+}) => {
+  return (
+    <StyledDiv {...rest}>
+      {children}
+    </StyledDiv>
+  )
+}
+
+const StyledDiv = styled(Container)`
+    &&& {
+      ${({ align }) => align ? `align-items: ${align}` : ''};
+      ${({ display }) => display ? `display: ${display}` : ''};
+      ${({ justify }) => justify ? `justify-content: ${justify}` : ''};
+    }
+`
--- src/components/common/Text.js
+++ src/components/common/Text.js
@@ -6,8 +6,9 @@ import { Typography } from '@material-ui/core'
 export const Text = ({
   align = 'center',
   children,
+  component,
   variant = 'body1',
   ...rest
 }) => {
-  return <Typography align={align} variant={variant} {...rest}>{children}</Typography>
+  return <Typography align={align} component={component} variant={variant} {...rest}>{children}</Typography>
 }
