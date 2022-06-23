49bae564f9f1d9ac57ae9571893a9592256ead80
carlosguerra1997 <guerragcarlos97@gmail.com>
2022-06-23 17:35:07 +0200

    Sign in & sign up actions

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,10 +1,19 @@
 import { SIGN_UP } from '../constants/actionTypes'
 
-import { signUpUser } from '../api/Auth'
+import { signInUser, signUpUser } from '../api/Auth'
 
-export const signUp = (values) => async dispatch => {
+export const signIn = (authInfo) => async dispatch => {
   try {
-    await signUpUser(values)
+    await signInUser(authInfo)
+    dispatch({ type: SIGN_UP })
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+export const signUp = (authInfo) => async dispatch => {
+  try {
+    await signUpUser(authInfo)
     dispatch({ type: SIGN_UP })
   } catch (error) {
     throw new Error(error)
--- src/api/Auth.js
+++ src/api/Auth.js
@@ -1,11 +1,11 @@
 import { clienteAxios } from '../config/axios'
 
-export const signInUser = async (post) => {
-  const data = await clienteAxios.post('/auth/signin', post)
+export const signInUser = async (authInfo) => {
+  const data = await clienteAxios.post('/auth/signin', authInfo)
   return data
 }
 
-export const signUpUser = async (post) => {
-  const data = await clienteAxios.post('/auth/signup', post)
+export const signUpUser = async (authInfo) => {
+  const data = await clienteAxios.post('/auth/signup', authInfo)
   return data
 }
