e8bf25bae5376df9f9d934a9eaf9200a1cd0c8a0
carlosguerra1997 <guerragcarlos97@gmail.com>
2022-06-24 12:23:48 +0200

    Axios interceptor req header

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,3 +3,10 @@ import axios from 'axios'
 export const API = axios.create({
   baseURL: process.env.REACT_APP_BASE_LOCAL_URL
 })
+
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem('profile')) {
+    req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
+  }
+  return req
+})
