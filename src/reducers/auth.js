9eeb340780ed4fe731fec111e946b985a1da086b
carlosguerra1997 <guerragcarlos97@gmail.com>
2022-06-24 12:24:31 +0200

    Auth reducers

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,16 +1,28 @@
-import { LOGOUT } from '../constants/actionTypes'
+import { CHECK_IF_SIGNED_IN, LOGOUT, SIGN_IN, SIGN_UP } from '../constants/actionTypes'
 
 const initialState = {
-  user: {}
+  user: null
 }
 
 export const authReducer = (state = initialState, action) => {
   switch (action.type) {
+    case CHECK_IF_SIGNED_IN:
+      return {
+        ...state,
+        user: action.payload
+      }
+    case SIGN_IN:
+    case SIGN_UP:
+      localStorage.setItem('profile', JSON.stringify({ ...action.payload }))
+      return {
+        ...state,
+        user: action.payload.userInfo
+      }
     case LOGOUT:
       localStorage.clear()
       return {
-        user: {},
-        ...state
+        ...state,
+        user: null
       }
     default:
       return state
