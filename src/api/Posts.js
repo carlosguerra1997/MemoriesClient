6b3c1021918fee4e0430cfd142928f0a009df025
carlosguerra1997 <guerragcarlos97@gmail.com>
2022-07-03 15:48:09 +0200

    Fetcj posts by search api call

--- src/api/Posts.js
+++ src/api/Posts.js
@@ -1,7 +1,8 @@
 import { API } from '../config/axios'
 
-export const addNewPost = async (post) => await API.post('/posts/', post)
+export const addNewPost = async post => await API.post('/posts/', post)
 export const getPosts = async () => await API.get('/posts/')
+export const fetchPostsBySearch = async searchQuery => await API.get(`/posts/search?searchQuery=${searchQuery.searchMemories}&tags=${searchQuery.tags}`)
 export const updatePost = async (postId, post) => await API.put(`/posts/${postId}`, post)
-export const likePost = async (postId) => await API.put(`/posts/${postId}/like`)
-export const deletePost = async (postId) => await API.delete(`/posts/${postId}`)
+export const likePost = async postId => await API.put(`/posts/${postId}/like`)
+export const deletePost = async postId => await API.delete(`/posts/${postId}`)
