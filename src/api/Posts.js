58f6e0256975e759caac8ceea496db9c6d1a28ff
carlosguerra1997 <guerragcarlos97@gmail.com>
2022-07-04 11:09:36 +0200

    Api call fix & dispatch action

--- src/api/Posts.js
+++ src/api/Posts.js
@@ -2,7 +2,7 @@ import { API } from '../config/axios'
 
 export const addNewPost = async post => await API.post('/posts/', post)
 export const getPosts = async () => await API.get('/posts/')
-export const fetchPostsBySearch = async searchQuery => await API.get(`/posts/search?searchQuery=${searchQuery.searchMemories}&tags=${searchQuery.tags}`)
+export const fetchPostsBySearch = async searchQuery => await API.get(`/posts/search?searchQuery=${searchQuery.searchMemories}`)
 export const updatePost = async (postId, post) => await API.put(`/posts/${postId}`, post)
 export const likePost = async postId => await API.put(`/posts/${postId}/like`)
 export const deletePost = async postId => await API.delete(`/posts/${postId}`)
--- src/components/common/AppBar.js
+++ src/components/common/AppBar.js
@@ -3,17 +3,20 @@ import styled from 'styled-components'
 
 import { Formik } from 'formik'
 
+import { useDispatch } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 // import { useGetPage } from '../../../hooks/useGetPage'
 // import { useSearchQuery } from '../../../hooks/useSearchQuery'
 
-import { AppBar } from '@material-ui/core'
+import { getPostsBySearch } from '../../actions/posts'
 
+import { AppBar } from '@material-ui/core'
 import { Buttons } from './Button'
 import { ChipInputs } from './ChipInput'
 import { TextFields } from './TextField'
 
 export const AppBars = () => {
+  const dispatch = useDispatch()
   const history = useHistory()
 
   const initialValues = {
@@ -23,7 +26,8 @@ export const AppBars = () => {
 
   const handleSubmit = (values) => {
     if (values.searchMemories.trim()) {
-      console.log(values)
+      const { searchMemories } = values
+      dispatch(getPostsBySearch({ searchMemories }))
     } else {
       history.push('/')
     }
