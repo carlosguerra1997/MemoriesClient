91a0b4a831ddb614d8587b2fa5b60b8e22a70015
carlosguerra1997 <guerragcarlos97@gmail.com>
2022-07-05 14:17:53 +0200

    New loading reducer

---
+++ src/reducers/loading.js
@@ -0,0 +1,12 @@
+// import { CHECK_IF_SIGNED_IN, LOGOUT, SIGN_IN, SIGN_UP } from '../constants/actionTypes'
+
+const initialState = {
+  isLoading: false
+}
+
+export const loadingReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import { applyMiddleware, createStore, combineReducers, compose } from 'redux'
 import thunk from 'redux-thunk'
 
 import { authReducer } from '../reducers/auth'
+import { loadingReducer } from '../reducers/loading'
 import { postsReducer } from '../reducers/posts'
 
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  loading: loadingReducer,
   posts: postsReducer
 })
 
