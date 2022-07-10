6ae60b0fbf8a4d3ebbf4c2e9757090f0c98b9242
Carlos Guerra <guerragcarlos97@gmail.com>
2022-07-10 18:49:26 +0200

    Post details layout

--- src/components/sections/Posts/details/PostDetails.js
+++ src/components/sections/Posts/details/PostDetails.js
@@ -1,6 +1,9 @@
 import React from 'react'
+// import { useSelector } from 'react-redux'
 
 export const PostDetails = () => {
+  // const { post } = useSelector(state => state.posts)
+
   return (
     <div>Post Details</div>
   )
--- src/components/sections/Posts/details/PostDetailsLayout.js
+++ src/components/sections/Posts/details/PostDetailsLayout.js
@@ -1,3 +1,4 @@
+import { Container } from '@material-ui/core'
 import React from 'react'
 
 import { Navbar } from '../../../common/navbar/Navbar'
@@ -5,9 +6,9 @@ import { PostDetails } from './PostDetails'
 
 export const PostDetailsLayout = () => {
   return (
-        <>
+        <Container maxWidth='xl'>
             <Navbar />
             <PostDetails />
-        </>
+        </Container>
   )
 }
