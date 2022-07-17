ab8c642ce0e55f99ee6a28c845f566d56c86b8a8
Carlos Guerra <guerragcarlos97@gmail.com>
2022-07-17 16:11:42 +0200

    Comments layout

--- src/components/sections/Posts/details/comment/Comments.js
+++ src/components/sections/Posts/details/comment/Comments.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
-export const Comments = () => {
+import { Containers } from '../../../../common/Container'
+import { Text } from '../../../../common/Text'
+
+export const Comments = ({ post }) => {
+  console.log('Post... ', post)
+
   return (
-    <div>Comments</div>
+    <Containers>
+      <Containers>
+        <Containers>
+          <Text variant='h6'>Comments</Text>
+        </Containers>
+      </Containers>
+    </Containers>
   )
 }
