01d7c45e8c0aa948d620cb47fe9d0cf80ea7b3ff
Carlos Guerra <guerragcarlos97@gmail.com>
2022-07-18 15:34:13 +0200

    Comments form

--- src/components/sections/Posts/details/comment/Comments.js
+++ src/components/sections/Posts/details/comment/Comments.js
@@ -1,15 +1,24 @@
+import styled from 'styled-components'
 import React, { useEffect, useState } from 'react'
 
-// import { Containers } from '../../../../common/Container'
+import { Formik } from 'formik'
+
+import { Containers } from '../../../../common/Container'
+import { TextFields } from '../../../../common/TextField'
 import { Text } from '../../../../common/Text'
 
 export const Comments = ({ post }) => {
+  const initialValues = { newComment: '' }
   const [comments, setComments] = useState([1, 2, 3, 4])
   console.log('Post... ', post)
 
   useEffect(() => {
     setComments([1, 2, 3, 4])
   }, [])
+
+  const handleSubmit = (values, resetForm) => {
+    resetForm()
+  }
 
   return (
     <div>
@@ -22,7 +31,19 @@ export const Comments = ({ post }) => {
             ))
           }
         </div>
+        <Formik initialValues={initialValues} onSubmit={(values, { resetForm }) => handleSubmit(values, resetForm)}>
+          {({ handleSubmit }) => (
+            <StyledContainer>
+              <Text gutterBottom variant='h6'>Deja un comentario</Text>
+              <TextFields fullWidth name='newComment' placeholder='Escribe un comentario...' />
+            </StyledContainer>
+          )}
+        </Formik>
       </div>
     </div>
   )
 }
+
+const StyledContainer = styled(Containers)`
+  width: 70%;
+`
